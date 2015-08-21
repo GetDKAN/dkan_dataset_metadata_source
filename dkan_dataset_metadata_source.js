3d0cdc723ecdac7d69bf83a29815e575d33dc28e
acouch <acinternets@gmail.com>
2015-08-21 17:55:57 +0000

    Updates metadata on form page

---
+++ dkan_dataset_metadata_source.js
@@ -0,0 +1,19 @@
+(function ($) {
+
+  Drupal.behaviors.dkanDatasetMetaSource = {
+    attach: function (context, settings) {
+      var height = 150;
+      var offset = height - 20;
+      // Adds "Show more" to rows longer than height.
+      $('#edit-field-metadata-file a.filefield-source-remotefile').click(function(e) {
+        console.log('clicked hide');
+        $('input[name="field_metadata_file_und_0_upload_button"]').hide();
+      });
+      $('#edit-field-metadata-file a.filefield-source-upload').click(function(e) {
+console.log('clicked');
+        $('input[name="field_metadata_file_und_0_upload_button"]').show();
+      });
+    }
+  }
+
+})(jQuery);
